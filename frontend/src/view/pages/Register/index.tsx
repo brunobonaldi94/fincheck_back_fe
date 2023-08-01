a0d77d89ad9f882ee4bda345e755c287886cd8aa
brunobonaldi94 <brunobonaldi94@gmail.com>
2023-07-31 23:01:47 -0300

    added login and register frontend

--- frontend/src/view/pages/Register/index.tsx
+++ frontend/src/view/pages/Register/index.tsx
@@ -1,3 +1,37 @@
+import { Link } from "react-router-dom";
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
 export function Register() {
-	return <h1>Register page</h1>
+	return (
+		<>
+			<header className="flex flex-col items-center gap-4 text-center">
+				<h1 className="text-2xl font-bold text-gray-900 tracking-[-1px]">Entre em sua conta</h1>
+				<p className="space-x-2">
+					<span className="text-gray-700 tracking-[-0.5px]">Novo por aqui?</span>
+					<Link to="/login" 
+						className="font-medium text-teal-900 tracking-[-0.5px]">
+						Fazer login
+					</Link>
+				</p>
+			</header>
+			<form className="mt-[60px] flex flex-col gap-4">
+				<Input 
+					type="text" 
+					placeholder="Nome" 
+					name="name"
+					/>
+				<Input 
+					type="email" 
+					placeholder="E-mail" 
+					name="email"
+					/>
+				<Input 
+					type="password" 
+					placeholder="Senha" 
+					name="password"/>
+				<Button type="submit" >Criar Conta</Button>
+			</form>
+		</>
+	)
 }
